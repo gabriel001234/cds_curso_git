d783897add439b37930d14f6094be75468e3de5f
Gabriel Paneque Didi <gabrielpanequedidi@gmail.com>
2023-04-22 23:52:09 -0300

    Corrigido erro de escrita no botao de download

--- src/answers.py
+++ src/answers.py
@@ -250,7 +250,7 @@ def rd3_question_7(df):
     df_xlsx = to_excel(df_selected)
 
     st.download_button(
-        label="📥 Download Buyinbg Suggestions",
+        label="📥 Download Buying Suggestions",
         data=df_xlsx,
         file_name="buing_suggestions.xlsx",
     )
